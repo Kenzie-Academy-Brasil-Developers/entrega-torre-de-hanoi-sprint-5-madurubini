4918745961b84c270092e5d2a720e10fdc4fbcbb
Julia Gama <gamaj235@gmail.com>
2021-08-02 16:18:04 -0300

    adicionado função criar torres estilizado

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("oi estou conectado")
+//function criarTorre()
+function criarTorre(){
+    let torre = document.createElement('div')
+    let baseTorre = document.createElement('div')
+    let secao = document.getElementById('jogo')
+    torre.className = "torres"
+    baseTorre.className = "base"
+    document.body.appendChild(secao)
+    secao.appendChild(torre)
+    torre.appendChild(baseTorre)
+}
+for(let i = 0; i < 3; i++){
+    criarTorre()
+}
