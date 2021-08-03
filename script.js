f2260729c911bda5abf10678c5b28f07c3703c68
Madu Rubini <mariaed.rubini@gmail.com>
2021-08-02 23:23:42 -0300

    Adição da função que cria discos

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-//function criarTorre()
+//Função que cria as torres 
+let torre = ""
+let baseTorre = ""
+
 function criarTorre(){
-    let torre = document.createElement('div')
-    let baseTorre = document.createElement('div')
+    torre = document.createElement('div')
+    baseTorre = document.createElement('div')
     let secao = document.getElementById('jogo')
     torre.className = "torres"
     baseTorre.className = "base"
@@ -9,6 +12,34 @@ function criarTorre(){
     secao.appendChild(torre)
     torre.appendChild(baseTorre)
 }
+
+
+
 for(let i = 0; i < 3; i++){
-    criarTorre()
+    criarTorre()   
+  
 }
+
+
+
+//Função criar disco
+
+function criarDisco(n){
+    let discoWidth = ""
+    for(let i = 0; i < n; i++){
+        let disco = document.createElement('div')
+        disco.className = "discos"
+        let numero = document.createTextNode(i+1)
+        disco.appendChild(numero)
+        baseTorre.appendChild(disco) 
+        discoWidth = disco.clientWidth  
+        disco.style.width = discoWidth + 10          
+    }
+    
+    
+}
+function posicionarDisco(){
+    criarDisco(3)
+}
+
+posicionarDisco()
