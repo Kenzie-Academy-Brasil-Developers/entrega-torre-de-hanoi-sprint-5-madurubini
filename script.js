bd1199daacc54bd235882d9a6a8f4b5a9e3c2b77
Madu Rubini <mariaed.rubini@gmail.com>
2021-08-03 15:32:50 -0300

    Adição e estilização dos discos via DOM

--- script.js
+++ script.js
@@ -1,45 +1,64 @@
 //Função que cria as torres 
-let torre = ""
-let baseTorre = ""
-
-function criarTorre(){
-    torre = document.createElement('div')
-    baseTorre = document.createElement('div')
-    let secao = document.getElementById('jogo')
-    torre.className = "torres"
-    baseTorre.className = "base"
-    document.body.appendChild(secao)
-    secao.appendChild(torre)
-    torre.appendChild(baseTorre)
-}
+// function criarTorre(){
+//  let barra = document.createElement('div')
+//  let torre = document.getElementsByClassName("torres")
+//  barra.className = "base"
+//  torre.appendChild(barra)
+    
+// }
 
 
 
-for(let i = 0; i < 3; i++){
-    criarTorre()   
+// for(let i = 0; i < 3; i++){
+//     criarTorre()   
   
-}
+// }
 
 
 //Função criar disco
-
-function criarDisco(n){
-    let discoWidth = ""
-    for(let i = 0; i < n; i++){
-        let disco = document.createElement('div')
-        disco.className = "discos"
-        let numero = document.createTextNode(i+1)
-        disco.appendChild(numero)
-        baseTorre.appendChild(disco) 
-        discoWidth = disco.clientWidth  
-        disco.style.width = discoWidth + 10          
-    }
+//  let disco = ""
+// function criarDisco(){
+   
+//     for(let i = 0; i < 3; i++){
+//         disco = document.createElement('div')
+//         disco.className = "discos"
+//         let torreInicial = document.getElementById("inicial")
+//         let numero = document.createTextNode(i+1)
+//         disco.appendChild(numero)
+//         torreInicial.appendChild(disco)        
+//     }
     
     
-}
-function posicionarDisco(){
-    criarDisco(3)
-}
+// }
+// function posicionarDisco(){
+//     criarDisco()
+//     let torreInicial = document.getElementById("inicial")
+//       let discoWidth = disco.clientWidth
+//       discoWidth += 20
+//       disco.style.width = discoWidth +"10px"
+     
+      
+//       console.log(torreInicial.childElementCount)
+// }
 
-posicionarDisco()
+// posicionarDisco()
 
+const disco1 = document.createElement('div')
+disco1.className = "disco1"
+let torreInicial = document.getElementById("inicial")
+torreInicial.appendChild(disco1)
+
+const disco2 = document.createElement('div')
+disco2.className = "disco2"
+torreInicial = document.getElementById("inicial")
+torreInicial.appendChild(disco2)
+
+const disco3 = document.createElement('div')
+disco3.className = "disco3"
+torreInicial= document.getElementById("inicial")
+torreInicial.appendChild(disco3)
+
+const disco4 = document.createElement('div')
+disco4.className = "disco4"
+torreInicial = document.getElementById("inicial")
+torreInicial.appendChild(disco4)
