1911d983f934644111e0ea72fd6792bc74d21142
Madu Rubini <mariaed.rubini@gmail.com>
2021-08-04 09:56:48 -0300

    Função de movimentar os discos

--- script.js
+++ script.js
@@ -6,17 +6,14 @@ torreInicial.appendChild(disco1)
 
 const disco2 = document.createElement('div')
 disco2.className = "disco2"
-torreInicial = document.getElementById("inicial")
 torreInicial.appendChild(disco2)
 
 const disco3 = document.createElement('div')
 disco3.className = "disco3"
-torreInicial= document.getElementById("inicial")
 torreInicial.appendChild(disco3)
 
 const disco4 = document.createElement('div')
 disco4.className = "disco4"
-torreInicial = document.getElementById("inicial")
 torreInicial.appendChild(disco4)
 
 let firstClick  
