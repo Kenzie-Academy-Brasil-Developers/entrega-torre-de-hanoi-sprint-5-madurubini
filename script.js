16af0aecd2f3b8eadab4361422c3003ea263e204
Madu Rubini <mariaed.rubini@gmail.com>
2021-08-04 13:38:06 -0300

    Adição da condição de vitória, estilização do jogo

--- script.js
+++ script.js
@@ -1,30 +1,37 @@
+let torreInicial = document.getElementById("inicial")
+let torreMeio = document.getElementById("meio")
+let torreFinal = document.getElementById("final")
+let popup = document.getElementsByClassName("popup")
+
+
+
+
+
 
 const disco1 = document.createElement('div')
 disco1.className = "disco1"
-let torreInicial = document.getElementById("inicial")
 torreInicial.appendChild(disco1)
 
 const disco2 = document.createElement('div')
 disco2.className = "disco2"
-torreInicial = document.getElementById("inicial")
 torreInicial.appendChild(disco2)
 
 const disco3 = document.createElement('div')
 disco3.className = "disco3"
-torreInicial= document.getElementById("inicial")
 torreInicial.appendChild(disco3)
 
 const disco4 = document.createElement('div')
 disco4.className = "disco4"
-torreInicial = document.getElementById("inicial")
 torreInicial.appendChild(disco4)
+
+
 
 let firstClick  //primeiro click
 let secondClick //segundo click 
 let firstQtd // primeira quantidade
 let secondQtd// segunda quantidade
-let lastElementOfFirst
-let lastElementOfSecond
+let lastElementOfFirst 
+let lastElementOfSecond 
 function game(tower) {
     let element = document.getElementById(tower)
     if(firstClick === undefined){
@@ -34,7 +41,7 @@ function game(tower) {
         firstQtd = firstClick.childElementCount
         lastElementOfFirst = firstClick.lastElementChild
         if(lastElementOfFirst == null){
-            alert('não pode ser em local vazio')
+            alert('O primeiro click não pode ser em um local vazio!')
             firstClick.style.border = '2px solid transparent'
             firstClick = undefined
             lastElementOfFirst  = undefined
@@ -51,11 +58,11 @@ function game(tower) {
         secondClick.appendChild(lastElementOfFirst)
         reset()
     } else {
-        alert('Coloque em um local válido')
+        alert('Este disco não é válido, por favor escolha outra torre')
         reset()
     }
 
-    
+    condicaoVitoria()
 }
 
 function reset() {//resetando
@@ -68,3 +75,11 @@ function reset() {//resetando
     firstQtd = undefined
     secondQtd = undefined
 }
+
+function condicaoVitoria(){
+    if(torreFinal.childElementCount === 4){
+        alert("Parabéns você Venceu")
+    }
+}
+
+
